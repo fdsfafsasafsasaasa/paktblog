4f046cea431cbed3a923bd81e7f0d900adafaccb
thewarsawpakt <smallperson07@gmail.com>
2020-12-23 17:06:59 -0600

    Switched to a more sane system for getting creds

--- PaktBlog/__init__.py
+++ PaktBlog/__init__.py
@@ -3,12 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from flask import Flask
 
-import json
 import os
-
-with json.loads(open("environment.json", "r")) as env:
-    for key in env:
-        os.environ[key] = env['key']
 
 app = Flask(__name__)
 app.secret_key = os.environ['FLASK_APP_KEY']
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 export PYTHONDONTWRITEBYTECODE=1
 
+export FLASK_APP_KEY='gentoo sucks'
+
+
 python3 run.py
